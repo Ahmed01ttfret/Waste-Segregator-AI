0b76ad06b92a32b460ed1e0d628df62ce0569799
Ahmed01ttfret <90077396+Ahmed01ttfret@users.noreply.github.com>
2024-09-30 21:19:04 +0000

    index.py

--- index.py
+++ index.py
@@ -1,3 +1,9 @@
+import pkg_resources
+installed_packages = pkg_resources.working_set
+installed = sorted(["%s==%s" % (i.key, i.version) for i in installed_packages])
+print(installed)
+
+
 import io
 import streamlit as st
 from PIL import Image
